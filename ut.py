906b6c6b6a9f6e3a5c1af89b1707081c4424773c
jx <33095108+JxBetter@users.noreply.github.com>
2019-01-01 22:43:50 +0800

    Add files via upload

--- ut.py
+++ ut.py
@@ -1,24 +1,40 @@
-from selenium import webdriver
+import HTMLTestRunner
 import unittest
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 
 
 class WebTest(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
-        # self.driver.implicitly_wait(10)
-        # self.driver.get('http://easythink.top')
+        self.driver.implicitly_wait(10)
+        self.driver.get('http://easythink.top')
 
-    def test_es(self):
-        # self.driver.find_element_by_css_selector('body > div.navbar.navbar-default > div > div.navbar-header > a')
-        # self.assertEquals(self.driver.title, 'EasyThink2', 'title error')
-        self.help()
+    def test_open(self):
+        self.assertEquals(self.driver.title, 'EasyThink', 'title error')
 
-    def help(self):
-        print('hhh')
+    def test_login(self):
+        login_btn = self.driver.find_element_by_css_selector('#down > ul.nav.navbar-nav.navbar-right > li > a')
+        login_btn.click()
+        ac = ActionChains(self.driver)
+        name = self.driver.find_element_by_css_selector('#username_email')
+        pwd = self.driver.find_element_by_css_selector('#password')
+        login = self.driver.find_element_by_css_selector('#submit')
+        ac.send_keys_to_element(name, 'ppp').send_keys_to_element(pwd, 'qqq').click(login).perform()
+        self.assertTrue('login' not in self.driver.current_url)
 
     def tearDown(self):
-        print('teardown')
+        self.driver.close()
 
 
 if __name__ == '__main__':
-    unittest.main()
+    suite = unittest.TestSuite()
+    suite.addTests([WebTest("test_open"), WebTest("test_login")])
+
+    with open('./res.html', 'wb') as f:
+        runner = HTMLTestRunner.HTMLTestRunner(
+            stream=f,
+            title='blog test',
+            verbosity=2
+        )
+        runner.run(suite)
